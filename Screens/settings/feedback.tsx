35194e010fdc047e0f9f16a0057290491e2bbe7b
Abel Tattah <122866505+AbelTattah@users.noreply.github.com>
2024-06-03 18:51:17 +0000

    Update Feedback Screen

--- Screens/settings/feedback.tsx
+++ Screens/settings/feedback.tsx
@@ -1,15 +1,102 @@
-import { View, Text, Touchable } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import {View, Text, TextInput, StyleSheet, Image} from 'react-native';
+import React, { useState } from 'react';
+import {TouchableOpacity} from 'react-native';
+import axios from 'axios';
 
 const Feedback = () => {
-  return (
-    <View>
-      <Text>Feedback</Text>
-      <TouchableOpacity>
-      </TouchableOpacity>
-    </View>
-  )
-}
+  let emotion:string = '';
+  const [suggestion, setSuggestion] = useState<string>()
 
-export default Feedback
+  async function sendFeedback() {
+    try {
+      axios.post('', {
+        feedback: emotion,
+        suggestion:suggestion
+      });
+    } catch (error) {}
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>User Feedback</Text>
+      <View>
+        <Text>What was your experience?</Text>
+        <View style={styles.emojis}>
+          <TouchableOpacity onPress={() => emotion = 'sad'}>
+            <Image
+              source={require('../../assets/sad.png')}
+              style={styles.emoji}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => emotion =  'dissapointed'}>
+            <Image
+              source={require('../../assets/dissapointment.png')}
+              style={styles.emoji}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => emotion = 'happy'}>
+            <Image
+              source={require('../../assets/happy.png')}
+              style={styles.emoji}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => emotion = 'love'}>
+            <Image
+              source={require('../../assets/love.png')}
+              style={styles.emoji}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <Text style={styles.suggestionsHeader}>Any Suggestions?</Text>
+        <TextInput style={styles.suggestion}></TextInput>
+        <TouchableOpacity onPress={()=>sendFeedback()} style={styles.send}></TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Feedback;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  emojis: {
+    flexDirection: 'row',
+    height: '30%',
+    gap: 17,
+  },
+  emoji: {
+    width: 50,
+    height: 48,
+  },
+  suggestionsHeader: {
+    color: '#ddd',
+  },
+  suggestion: {
+    borderBottomWidth: 2,
+    borderStyle: 'solid',
+    height:200,
+    width:'80%'
+  },
+  send:{
+    width: 300,
+    height: 86,
+    backgroundColor: "#fff",
+    borderColor: "#00f",
+    elevation:3,
+    borderWidth:0.3,
+    borderRadius: 30,
+    justifyContent: "center",
+    margin: 10,
+    marginRight: 10,
+  }
+});
